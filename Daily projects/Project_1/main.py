f452c7ecf8fc8db1f1a389eca4b3b94471bf723b
LuisJoseSC <lj.sanchezc1@uniandes.edu.co>
2026-02-02 11:35:22 -0500

    Gitignore for .venv

---
+++ Daily projects/Project_1/main.py
@@ -0,0 +1,9 @@
+#Poryecto 1, machine learning.
+
+#Aun no esta claro, repaso de eda basicas.
+
+
+lst = []
+for i in range(1, 10):
+    lst.append()
+print(lst)
---
+++ Daily projects/Project_2/main.py
@@ -0,0 +1,4 @@
+#Se hara un proyecto sobre Estructuras de datos.
+#Este sera una carpeta donde estaran todas las estructuras y modos de ordenamiento existentes en la industria.
+#Bibliografia: Libro: 50 algoritmos que todo programador debe saber.
+
