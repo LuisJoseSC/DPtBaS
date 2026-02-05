3c264d44d95d03607b6bfa0cd412461dbedd9720
LuisJoseSC <lj.sanchezc1@uniandes.edu.co>
2026-02-05 18:12:59 -0500

    Proyecto_1

--- Daily projects/Project_2/main.py
+++
@@ -1,4 +0,0 @@
-#Se hara un proyecto sobre Estructuras de datos.
-#Este sera una carpeta donde estaran todas las estructuras y modos de ordenamiento existentes en la industria.
-#Bibliografia: Libro: 50 algoritmos que todo programador debe saber.
-
